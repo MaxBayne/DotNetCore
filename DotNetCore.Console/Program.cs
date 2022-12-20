f071799e9e238698604867280601c02fbd8777c8
Mohamed Salah <maxbayne@gmail.com>
2022-12-20 13:36:11 +0200

    Add project files.

---
+++ DotNetCore.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DotNetCore.Console
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! , Iam Max Bayne");
+        }
+    }
+}
