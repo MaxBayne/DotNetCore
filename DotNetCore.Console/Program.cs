58c03f3c04f800bb26886148eb7a757792cea12c
Mohamed Salah <maxbayne@gmail.com>
2022-12-20 16:27:05 +0200

    create xunit Test Project

--- DotNetCore.Console/Program.cs
+++ DotNetCore.Console/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace DotNetCore.Console
+﻿using System.Globalization;
+using Humanizer;
+
+namespace DotNetCore.Console
 {
     using System;
 
@@ -6,7 +9,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World! , Iam Max Bayne");
+            //int counter = 10;
+
+            //for (int i = 0; i < counter; i++)
+            //{
+            //    Console.WriteLine($"Hello, World ! , Iam Max Bayne {i} \n");
+            //}
+
+            var result = 1520.ToWords(new CultureInfo("ar"));
+            Console.WriteLine(result);
+
         }
     }
 }
---
+++ DotNetCore.XUnitTestes/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace DotNetCore.XUnitTestes
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
